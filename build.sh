747542178140a7776b0a5d6fea4d9b5a95ef9449
Karl Krukow <karl.krukow@gmail.com>
2012-03-20 19:15:38 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p build
 java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/calabash.js > calabash-min.tmp.js
 java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/set_text.js > set_text-min.tmp.js
 
