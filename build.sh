fa5ccea56fcc7a45f22f1579f29ca2e312ccfd5a
Joshua Moody <joshuajmoody@gmail.com>
2015-02-20 17:02:39 +0100

    Script should clean products before building

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+
+rm -rf build
 mkdir -p build
+rm -f *-min.tmp.js
+
 java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/calabash.js > calabash-min.tmp.js
 java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/set_text.js > set_text-min.tmp.js
 
 sed "s/\"/'/g" calabash-min.tmp.js > build/calabash-min.js
 sed "s/\"/'/g" set_text-min.tmp.js > build/set_text-min.js
-rm *-min.tmp.js
+
+rm -f *-min.tmp.js
+
