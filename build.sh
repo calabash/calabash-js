7093ca20244b817d0448c46137b9ba233911c072
Joshua Moody <joshuajmoody@gmail.com>
2015-02-20 17:17:33 +0100

    Confine build products to build dir

--- build.sh
+++ build.sh
@@ -2,13 +2,16 @@
 
 rm -rf build
 mkdir -p build
-rm -f *-min.tmp.js
 
-java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/calabash.js > calabash-min.tmp.js
-java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/set_text.js > set_text-min.tmp.js
+CALABASH_TMP_JS=build/calabash-min.tmp.js
+SET_TEXT_TMP_JS=build/set_text-min.tmp.js
 
-sed "s/\"/'/g" calabash-min.tmp.js > build/calabash-min.js
-sed "s/\"/'/g" set_text-min.tmp.js > build/set_text-min.js
+java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/calabash.js > $CALABASH_TMP_JS
+java -jar deps/yuicompressor-2.4.7.jar --type js --charset utf-8 -v --nomunge --preserve-semi --disable-optimizations src/set_text.js > $SET_TEXT_TMP_JS
 
-rm -f *-min.tmp.js
+sed "s/\"/'/g" $CALABASH_TMP_JS > build/calabash-min.js
+sed "s/\"/'/g" $SET_TEXT_TMP_JS > build/set_text-min.js
+
+rm -f $CALABASH_TMP_JS
+rm -f $SET_TEXT_TMP_JS
 
